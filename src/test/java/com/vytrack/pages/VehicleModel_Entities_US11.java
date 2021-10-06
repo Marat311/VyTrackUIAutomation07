33c395e1e81587a17228d0463cca6766ddc9ff3e
blishchenko1981 <blishchenko1981@gmail.com>
2021-10-06 12:22:20 -0400

    UserStory 11 Done

--- src/test/java/com/vytrack/pages/VehicleModel_Entities_US11.java
+++ src/test/java/com/vytrack/pages/VehicleModel_Entities_US11.java
@@ -1,4 +1,34 @@
 package com.vytrack.pages;
 
+import com.vytrack.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import java.util.List;
+
 public class VehicleModel_Entities_US11 {
+
+    @FindBy (xpath = "//label[starts-with(text(), 'Total of')]")
+    public WebElement totalModels;
+
+    @FindBy(xpath = "//tr/td[@class = 'string-cell grid-cell grid-body-cell grid-body-cell-ModelName']")
+    public List<WebElement> visibleTotal;
+
+
+    public VehicleModel_Entities_US11 (){
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+    public boolean compareActualvsTotal(){
+
+        String number = totalModels.getText().substring(totalModels.getText().indexOf("f")+2);
+                   number = number.substring(0,1);
+                   int numsOfModels = Integer.parseInt(number);
+
+           return numsOfModels == visibleTotal.size();
+    }
+
+
+
 }
--- src/test/java/com/vytrack/tests/UserStory11_AllVehicleModels.java
+++ src/test/java/com/vytrack/tests/UserStory11_AllVehicleModels.java
@@ -3,13 +3,18 @@ package com.vytrack.tests;
 import com.sun.jmx.snmp.SnmpUnknownAccContrModelException;
 import com.vytrack.pages.Dashbord_UserStory7_11;
 import com.vytrack.pages.LoginPage;
+import com.vytrack.pages.VehicleModel_Entities_US11;
+import com.vytrack.utilities.BrowserUtil;
+import com.vytrack.utilities.TestBase;
+import com.vytrack.utilities.VyTrackUtility;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
-public class UserStory11_AllVehicleModels {
+public class UserStory11_AllVehicleModels extends TestBase {
 //    AC #1: user should view all the vehicles model
 //    Given user is on the homePage
 //    When user select “Vehicle Model” under Fleet module
@@ -36,17 +41,20 @@ public class UserStory11_AllVehicleModels {
         for (String name : names) {
             loginPage.login(name);
 
+            BrowserUtil.waitFor(2);
        // When user select “Vehicle Model” under Fleet module
             Dashbord_UserStory7_11 dash = new Dashbord_UserStory7_11();
             dash.fleetlink.click();
+            BrowserUtil.waitFor(1);
+            dash.vehicleModelOption.click();
 
+       //    Then verify user can view all the vehicles
 
+            BrowserUtil.waitFor(5);
+            VehicleModel_Entities_US11 models = new VehicleModel_Entities_US11();
+            Assertions.assertTrue(models.compareActualvsTotal());
 
-
-
-
-
-
+            VyTrackUtility.logout();
         }
 
 
